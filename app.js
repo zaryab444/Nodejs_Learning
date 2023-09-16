287ba2745d83a441df073406f3f1930a689fc45e
zaryab <zaryabdeveloper49@gmail.com>
2023-09-16 12:00:06 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/add-product',(req,res,next) =>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit </button></form>');
+});
+
+app.use('/product',(req,res,next) =>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next) =>{
+   res.send('Main middleware'); 
+})
+app.listen(3000);
