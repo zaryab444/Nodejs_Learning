01ff5692d3d7bf33ad22324a48943e2a42f9ae4e
zaryab444 <zaryabsohail444@gmail.com>
2023-09-19 16:46:48 +0500

    product crud api work

--- controllers/product.js
+++ controllers/product.js
@@ -1,25 +1,66 @@
 const Product = require("../models/product");
 
-exports.getAddProduct = (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Submit </button></form>'
-  );
+//http://localhost:3000/admin/all-product (Get Request)
+exports.getAllProduct = async (req, res, next) => {
+  const products = await Product.find({});
+  res.json(products);
 };
 
-exports.postAddProduct = (req, res, next) => {
+//http://localhost:3000/admin/add-product (Post Request)
+exports.postAddProduct = async (req, res, next) => {
   let product = new Product({
     title: req.body.title,
     imageUrl: req.body.imageUrl,
     price: req.body.price,
     description: req.body.description,
   });
-  product
-    .save()
-    .then((result) => {
-      res.send("Product Created");
-    })
-    .catch((err) => {
-      res.send(err);
-      console.log(err);
-    });
+  product = await product.save();
+  res.status(200).json(product);
 };
+
+//http://localhost:3000/admin/product/productId (Get Request)
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req?.params?.productId);
+  if(product){
+    return res.json(product);
+  }
+  else {
+    res.status(404).json({message: 'Resource not found'});
+  }
+};
+
+//http://localhost:3000/admin/update-product/65098462db8bcb888d47f1e3 (Put request)
+exports.updateSingleProduct = async (req,res,next) =>{
+  const { title, imageUrl, price, description } =
+  req.body;
+  const product = await Product.findById(req.params.productId);
+  if (product) {
+    product.title = title;
+    product.imageUrl = imageUrl;
+    product.price = price;
+    product.description = description;
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  }
+  else {
+    res.status(404).json({message: 'Resource not found'});
+  }
+};
+
+//http://localhost:3000/admin/delete-product/productId 
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    await Product.deleteOne({ _id: product._id });
+
+    return res.status(200).json({ message: 'Product Deleted' });
+  } catch (error) {
+    return next(error);
+  }
+};
+
--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,14 @@ const productController = require('../controllers/product');
 
 const router = express.Router();
 
-// router.get('/add-product', productController.getAddProduct);
+ router.get('/all-products', productController.getAllProduct);
 
 router.post('/add-product',productController.postAddProduct);
 
+
+router.get('/product/:productId', productController.getSingleProduct);
+
+router.delete('/delete-product/:productId', productController.deleteProduct);
+
+router.put('/update-product/:productId', productController.updateSingleProduct);
 module.exports = router;
